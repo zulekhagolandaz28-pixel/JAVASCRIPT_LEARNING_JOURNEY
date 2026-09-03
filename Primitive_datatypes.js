03d76d85a2ec35cd3924ba21e04cc1b2ebff5f01
Zulekhagolandaz28 <zulekhagolandaz28@gmail.com>
2026-09-03 12:14:47 +0530

    Topic 1 - Priitive data types completed

---
+++ Primitive_datatypes.js
@@ -0,0 +1,37 @@
+// Q1 Create variables for your name, age, and whether you are a student. 
+// Print their values and data types.
+let name = "Zulekha"
+let age = 20
+let isstudent = true
+console.log(name)
+console.log(typeof(name))
+console.log(age)
+console.log(typeof(age))
+console.log(isstudent)
+console.log(typeof(isstudent))
+
+// Q2 Create these variables:
+//price = 499     //product = "Notebook"  //available = true   //discount = null
+//Print the value and data type of each variable.
+let price = 499
+let product = "Notebook"
+let available = true
+let discount = null
+console.log(price)
+console.log(typeof(price))
+console.log(product)
+console.log(typeof(product))
+console.log(available)
+console.log(typeof(available))
+console.log(discount)
+console.log(typeof(discount))
+
+//Predict the output before running the code:
+let a = "20";
+let b = 20;
+let c = true;
+let d;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
